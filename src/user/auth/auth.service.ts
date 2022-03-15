b36f467fce8b5553b234888a5e18066c66f5f976
RuiOkazaki <okazaki200226@gmail.com>
2022-03-15 16:03:36 +0900

    update: signupの処理を記述した

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
+
+type SignupParams = {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+};
 
 @Injectable()
 export class AuthService {
-  signup() {}
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email, password, name, phone }: SignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (userExists) throw new ConflictException();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+  }
 }
