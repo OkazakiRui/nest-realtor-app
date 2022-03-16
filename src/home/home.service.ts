ea2a7fee5f5dbea7356b96c029ec2959734e686c
RuiOkazaki <okazaki200226@gmail.com>
2022-03-16 10:13:22 +0900

    feat: 画像を一枚のみ取得するように変更

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -7,7 +7,30 @@ export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getHomes(): Promise<HomeResponseDto[]> {
-    const homes = await this.prismaService.home.findMany();
-    return homes.map((home) => new HomeResponseDto(home));
+    const homes = await this.prismaService.home.findMany({
+      select: {
+        id: true,
+        address: true,
+        city: true,
+        price: true,
+        property_type: true,
+        number_of_bedrooms: true,
+        number_of_bathrooms: true,
+        // 画像を一枚だけ取得する
+        images: {
+          // key が url のみ取得する
+          select: {
+            url: true,
+          },
+          take: 1,
+        },
+      },
+    });
+    return homes.map((home) => {
+      const fetchHome = { ...home, image: home.images[0].url };
+      // images と image が存在しているので imagesプロパティ を削除する
+      delete fetchHome.images;
+      return new HomeResponseDto(fetchHome);
+    });
   }
 }
