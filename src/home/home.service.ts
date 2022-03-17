721599029a83a53791ab2f4469c71b7a6ee9d9a1
RuiOkazaki <okazaki200226@gmail.com>
2022-03-17 21:10:31 +0900

    feat: authの情報と不動産に結びつけられたuserIdが一致する場合のみ実行されるようにした

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -157,4 +157,24 @@ export class HomeService {
     await this.prismaService.image.deleteMany({ where: { home_id: id } });
     await this.prismaService.home.delete({ where: { id } });
   }
+
+  async getRealtorByHomeId(id: number) {
+    // homeに結びつくrealtorからユーザー情報を取得する
+    const home = await this.prismaService.home.findUnique({
+      where: { id },
+      select: {
+        realtor: {
+          select: {
+            name: true,
+            id: true,
+            email: true,
+            phone: true,
+          },
+        },
+      },
+    });
+
+    if (!home) throw new NotFoundException();
+    return home.realtor;
+  }
 }
